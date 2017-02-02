0eae31e2ac13574d8855771575523b47860af336
rommoise <rommoise@amazon.com>
2017-02-02 09:59:19 -0800

    Fixing the coverage command

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const exec = require('child_process').exec;
-exec('./node_modules/istanbul/lib/cli.js cover ./node_modules/testing/all.js');
+require('child_process')
+  .exec(
+    'node ' +
+    './node_modules/istanbul/lib/cli.js' +
+    ' cover ' +
+    './node_modules/testing/all.js',
+    (e, out) => {
+      if (e) {
+        throw e;
+      }
+      console.log(out);
+    });
