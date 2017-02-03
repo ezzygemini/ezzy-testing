7cd9c1111ec842fd3d5c0f8a67c660c0e1a661fd
Moises Romero <moises@novacloudware.com>
2017-02-03 04:22:00 -0800

    Adding a cwd

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 require('child_process').exec(
   'node ' +
   './node_modules/istanbul/lib/cli.js' +
   ' cover ' +
   './node_modules/testing/all.js',
+  {
+    cwd: path.normalize(__dirname + '/../../'),
+    timeout: 900000 // 15 minute timeout
+  },
   (e, stdout, stderr) => {
     if (e) {
       console.error(e);
