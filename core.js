1b55577b7a377c336246d6805c7a12f9c41c1e11
rommoise <rommoise@lab126.com>
2017-02-06 11:42:00 -0800

    Checking folder on testing to avoid failures

--- core.js
+++ core.js
@@ -1,14 +1,15 @@
 const Jasmine = require('jasmine');
+const path = require('path');
+const fs = require('fs');
 
 /**
  * Core function that will execute a set of jasmine sets.
+ *
  * @param {string} type The type of testing to do.
  * @param {object} config The object configuration for the jasmine spec.
  * @returns {Promise.<void>}
  */
 const promiseFn = (type, config) => {
-  const jasmine = new Jasmine();
-
   if (typeof config === 'string') {
     type += `  (test/${config}/*.js)`;
     config = {
@@ -17,14 +18,34 @@ const promiseFn = (type, config) => {
     };
   }
 
-  jasmine.loadConfig(Object.assign({
-    'spec_dir': 'src',
-    'spec_files': []
-  }, config));
+  return new Promise((resolve, reject) => {
 
-  console.log(`Running: ${type}`);
+    fs.exists(path
+    // jscs:disable
+      .normalize(process.cwd() + '/' + config['spec_dir']), (e, exists) => {
+      // jscs:enable
 
-  return Promise.resolve(jasmine.execute());
+      if (e) {
+        return reject(e);
+      }
+
+      if (!exists) {
+        return resolve();
+      }
+
+      const jasmine = new Jasmine();
+
+      jasmine.loadConfig(Object.assign({
+        'spec_dir': 'test',
+        'spec_files': []
+      }, config));
+
+      console.log(`Running: ${type}`);
+
+      return resolve(jasmine.execute());
+    });
+
+  });
 };
 
 module.exports = promiseFn;
