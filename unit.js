4076ed2de54381ba0b49563ec77cc8a3b37b0c16
rommoise <rommoise@lab126.com>
2017-02-06 11:32:14 -0800

    Switching some folders

--- unit.js
+++ unit.js
@@ -1,16 +1,16 @@
 const core = require('./core');
-module.exports = core('unit testing (test/unit/*.js)', {
-  'spec_dir': 'test/unit',
-  'spec_files': [
-    '*.js',
-    '**/*.js'
-  ]
-}).then(() => core('unit testing (src/*Test.js)', {
+module.exports = core('unit testing (src/*Test.js)', {
   'spec_dir': 'src',
   'spec_files': [
     '*Test.js',
     '*Unit.js',
     '**/*Test.js',
     '**/*Unit.js'
+  ]
+}).then(() => core('unit testing (test/unit/*.js)', {
+  'spec_dir': 'test/unit',
+  'spec_files': [
+    '*.js',
+    '**/*.js'
   ]
 }));
