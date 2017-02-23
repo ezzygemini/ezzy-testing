bc7e7756904820baee1d0023535acf56121d3ba2
Moises Romero <moises@novacloudware.com>
2017-02-23 05:24:43 -0800

    Adding e2e testing process

--- e2e.js
+++ e2e.js
@@ -1,1 +1,20 @@
-module.exports = require('./core')('end-to-end testing', 'e2e');
+const exec = require('child_process').exec;
+const path = require('path');
+
+const log = (e, stdout, stderr) => {
+  if(e){
+    console.error(e);
+  }
+  if(stdout){
+    console.log(stdout);
+  }
+  if(stderr){
+    console.log(stderr);
+  }
+};
+
+exec('npm install -g protractor && webdriver-manager update', (...args) => {
+  log.apply(this, args);
+  exec('webdriver-manager start', log);
+  exec('protractor ' + path.normalize(__dirname + '/protractorConfig.js'), log);
+});
