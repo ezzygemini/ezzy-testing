9ca695e20cdbe9b0bdb11609ad93e397056a3511
rommoise <rommoise@amazon.com>
2017-02-23 16:35:58 -0800

    Improving configuration

--- protractorConfig.js
+++ protractorConfig.js
@@ -5,7 +5,10 @@ exports.config = Object.assign({
   seleniumServerJar: '../../node_modules/protractor/' +
   'node_modules/webdriver-manager/' +
   'selenium/selenium-server-standalone-3.1.0.jar',
-  specs: ['../../test/e2e/*Test.js'],
+  specs: [
+    '../../test/e2e/*Test.js',
+    '../../test/e2e/**/*Test.js'
+  ],
   baseUrl: 'http://localhost:' + (protractor.port || 9000) + '/',
   framework: 'jasmine',
   jasmineNodeOpts: {defaultTimeoutInterval: 30000},
