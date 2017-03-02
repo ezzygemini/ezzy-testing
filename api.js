164d11798bf63af2eae51df7a9d8b511a9635b3d
rommoise <rommoise@lab126.com>
2017-03-02 10:04:35 -0800

    API testing

--- api.js
+++ api.js
@@ -1,21 +1,23 @@
+module.exports = require('./core')('api testing', 'api');
+
 // module.exports = require('./protractor')('api');
-
-const exec = require('child_process').exec;
-const path = require('path');
-
-module.exports = new Promise((resolve, reject) => {
-  exec('node ./index', {
-    cwd: path.normalize(__dirname + '/test')
-  }, (e, stdout, stderr) => {
-    if (e) {
-      console.error(e);
-      reject(e);
-    }else if (stderr) {
-      console.error(stderr);
-      reject(stderr);
-    } else {
-      console.log(stdout);
-      resolve(stdout);
-    }
-  });
-});
+//
+// const exec = require('child_process').exec;
+// const path = require('path');
+//
+// module.exports = new Promise((resolve, reject) => {
+//   exec('node ./index', {
+//     cwd: path.normalize(__dirname + '/test')
+//   }, (e, stdout, stderr) => {
+//     if (e) {
+//       console.error(e);
+//       reject(e);
+//     }else if (stderr) {
+//       console.error(stderr);
+//       reject(stderr);
+//     } else {
+//       console.log(stdout);
+//       resolve(stdout);
+//     }
+//   });
+// });
